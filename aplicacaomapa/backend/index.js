66fdba97c8bd83ff425591a11ae4bbaf60ad763a
Juan Rodrigues <juanjrg2114@gmail.com>
2026-09-22 16:14:57 -0300

    Adicionando rederecionamento do mapa no front end

--- aplicacaomapa/backend/index.js
+++ aplicacaomapa/backend/index.js
@@ -1,8 +1,11 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import cors from 'cors'
 dotenv.config()
 const app = express()
 const port = 3000
+app.use(cors())
+app.use(express.json())
 
 import { Client } from 'pg'
 const client = await new Client({
@@ -16,7 +19,7 @@ const client = await new Client({
 
 app.get('/municipios/:codigo', async (req, res) => {
   const geojson = await client.query(
-    'select ST_ASGeoJSON(geom) from municipios where id = $1',[req.params.codigo]
+    'select ST_ASGeoJSON(geom) as geojson from municipios where id = $1', [req.params.codigo]
   )
   res.json(geojson.rows[0])
 })
